15a3788330643c80bd7adb381fb4cb4190505555
Zain Ahmad <zain.x.ahmad@gmail.com>
2019-03-04 19:15:24 +0100

    Consolidate camera into Camera class

--- app/src/Camera.h
+++ app/src/Camera.h
@@ -6,8 +6,20 @@
 #include "math/vector.hpp"
 
 #include <cmath>
+
+struct CameraSettings {
+  float screen_height{};
+  float screen_width{};
+  float aspect_ratio{};
+  float fov{};
+
+  float near{};
+  float far{};
+};
+
 class Camera {
-  math::matrix projection_matrix{};
+  CameraSettings settings{};
+  math::matrix projection{};
   math::vector camera_pos{};
   math::vector look_dir{};
 
@@ -15,78 +27,42 @@ class Camera {
   float theta{};
 
 public:
-  Camera(float fov, float aspect_ratio, float near, float far) {
-    const float fov_radians = 1.f / tanf(fov * 0.5 / 180.f * M_PI);
-    look_dir = {0, 0, 1};
-
-    projection_matrix(0, 0) = aspect_ratio * fov_radians;
-    projection_matrix(1, 1) = fov_radians;
-    projection_matrix(2, 2) = far / (far - near);
-    projection_matrix(3, 2) = (-far * near) / (far - near);
-    projection_matrix(2, 3) = 1.f;
-    projection_matrix(3, 3) = 0.f;
+  Camera(const CameraSettings &settings) : settings(settings) {
+    reconfigure(settings);
   }
 
-  void move(const math::vector &movement) { camera_pos += movement; }
-
-  void strafe(float rotation) { yaw += rotation; }
-
-  void look(const math::vector &look) { look_dir += look; };
-
-  math::matrix look_at(const math::vector &target, const math::vector &up) {
-    auto new_forward = target - camera_pos;
-    new_forward.normalize();
-
-    auto new_up = up - (new_forward * up.dot_product(new_forward));
-    new_up.normalize();
-
-    auto new_right = new_up.cross_product(new_forward);
-
-    math::matrix matrix;
-    matrix(0, 0) = new_right.x;
-    matrix(0, 1) = new_right.y;
-    matrix(0, 2) = new_right.z;
-
-    matrix(1, 0) = new_up.x;
-    matrix(1, 1) = new_up.y;
-    matrix(1, 2) = new_up.z;
-
-    matrix(2, 0) = new_forward.x;
-    matrix(2, 1) = new_forward.y;
-    matrix(2, 2) = new_forward.z;
-
-    matrix(3, 0) = camera_pos.x;
-    matrix(3, 1) = camera_pos.y;
-    matrix(3, 2) = camera_pos.z;
-    matrix(3, 3) = 1.f;
-
-    return matrix;
+  void reconfigure(const CameraSettings &settings) {
+    this->settings = settings;
+    projection = math::make_projection(settings.fov, settings.aspect_ratio,
+                                       settings.near, settings.far);
   }
 
   void transform(Mesh &mesh) {
-    auto rotation_z = math::make_rotation_z(theta * 0.5f);
-    auto rotation_x = math::make_rotation_x(theta);
-    auto translation = math::make_translation({0.f, 0.f, 5.f});
+    const auto rotZ = math::make_rotation_z(theta * 0.5f);
+    const auto rotX = math::make_rotation_x(theta);
 
-    auto world_matrix = rotation_z * rotation_x;
-    world_matrix *= translation;
+    const auto translation = math::make_translation({0.f, 0.f, 5.f});
+
+    auto world = rotZ * rotX;
+    world *= translation;
 
     math::vector up{0, 1, 0};
+    const auto cameraRot = math::make_rotation_y(yaw);
 
-    auto rotation_cam = math::make_rotation_y(yaw);
+    math::vector target{0, 0, 1};
 
-    auto look = math::multiply(look_dir, rotation_cam);
-    auto target = camera_pos + look;
+    look_dir = math::multiply(target, cameraRot);
+    target = camera_pos + look_dir;
 
-    auto cam = look_at(target, up);
-    auto view_matrix = math::inverse(cam);
+    const auto cameraMat = math::point_at(camera_pos, target, up);
+    const auto view = math::inverse(cameraMat);
 
-    mesh.rotate(world_matrix);
-    mesh.rotate(view_matrix);
-    mesh.rotate(projection_matrix);
+    mesh.rotate(world);
+    mesh.rotate(view);
+    mesh.rotate(projection);
 
     mesh.translate({1, 1, 0});
-    mesh.scale(Dimension::X, 0.5 * 800);
-    mesh.scale(Dimension::Y, 0.5 * 600);
+    mesh.scale(Dimension::X, settings.screen_width * 0.5f);
+    mesh.scale(Dimension::Y, settings.screen_height * 0.5f);
   }
 };
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -46,16 +46,14 @@ int main(int argc, char **argv) {
   float last_time = 0;
   sf::Clock clock;
 
-  Camera cam(90, 800 / 600, 0.1, 1000);
-
   PulsingMesh pulsing{1.5, 0.3, 1, false, {&cube}};
 
-  math::matrix projection =
-      math::make_projection(90.f, 800 / 600, 0.1f, 1000.f);
-  math::vector camera;
-  math::vector lookDir;
-  float yaw{};
-  float theta{};
+  Camera camera(CameraSettings{.screen_height = 800,
+                               .screen_width = 600,
+                               .aspect_ratio = 800 / 600,
+                               .fov = 90.f,
+                               .near = 0.1f,
+                               .far = 1000.f});
 
   while (App.isOpen()) {
     const auto current_time = clock.restart().asSeconds();
@@ -70,101 +68,32 @@ int main(int argc, char **argv) {
         App.close();
     }
 
-    const auto movement_speed = 10;
-    const auto forward = lookDir * (movement_speed * current_time);
-    math::vector target{0, 0, 1};
-
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-      camera.x -= movement_speed * current_time;
-
-      cam.move({-movement_speed * current_time, 0, 0});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-      camera.x += movement_speed * current_time;
-
-      cam.move({movement_speed * current_time, 0, 0});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-      camera.y -= movement_speed * current_time;
-
-      cam.move({0, -movement_speed * current_time, 0});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-      camera.y += movement_speed * current_time;
-
-      cam.move({0, movement_speed * current_time, 0});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
-      camera += forward;
-
-      cam.move({0, 0, movement_speed * current_time});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)) {
-      camera -= forward;
-
-      cam.move({0, 0, -movement_speed * current_time});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-      target.y += movement_speed * current_time;
-      cam.look({0, 1 * current_time, 0});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-      target.y -= movement_speed * current_time;
-
-      cam.look({0, -1 * current_time, 0});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-      yaw -= movement_speed / 4 * current_time;
-      cam.look({-1 * current_time, 0, 0});
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-      yaw += movement_speed / 4 * current_time;
-      cam.look({1 * current_time, 0, 0});
     }
 
     App.clear();
 
-    const auto rotZ = math::make_rotation_z(theta * 0.5f);
-    const auto rotX = math::make_rotation_x(theta);
+    camera.transform(cube);
 
-    const auto translation = math::make_translation({0.f, 0.f, 5.f});
-
-    auto world = rotZ * rotX;
-    world *= translation;
-
-    math::vector up{0, 1, 0};
-    const auto cameraRot = math::make_rotation_y(yaw);
-
-    lookDir = math::multiply(target, cameraRot);
-    target = camera + lookDir;
-
-    const auto cameraMat = math::point_at(camera, target, up);
-    const auto view = math::inverse(cameraMat);
-
-    auto quads = cube.vertices();
-    std::vector<Quad> to_draw;
-
-    for (auto &quad : quads) {
-      for (auto &vertex : quad.vertices) {
-        vertex = math::multiply(vertex, world);
-        vertex = math::multiply(vertex, view);
-        vertex = math::multiply(vertex, projection);
-
-        vertex = vertex / vertex.w;
-
-        vertex += math::vector{1, 1, 0};
-        vertex.x *= (0.5 * screenwidth);
-        vertex.y *= (0.5 * screenheight);
-      }
-    }
-
-    for (auto &line : Mesh::make_lines(quads)) {
-      for (auto &vertex : line) {
-        vertex.color = sf::Color::White;
-      }
-      App.draw(line.data(), 2, sf::Lines);
-    }
-
+    App.draw(cube);
     App.display();
   }
 }
